fb736971914fc00db3b5e14b168ae6174df5f7ab
Sabuj Kundu <manchumahara@gmail.com>
2016-07-28 22:10:39 +0600

    Adding proper trigger context for mod_articles_news
    mod_articles_news module doesn't use unique and proper context event trigger
    
    1. For showing after title it was used onContentBeforeDisplay which is confusing, it's better to use onContentAfterTitle
    
    2. Second issue I noticed that, context is used wrong, "com_content.article" context is used in core component and the module should use something different, otherwise it's no use of using context !
    
    So I changed context to "mod_articles_news.article"
    
    Thanks

--- modules/mod_articles_news/helper.php
+++ modules/mod_articles_news/helper.php
@@ -102,10 +102,10 @@ abstract class ModArticlesNewsHelper
 				$item->introtext = preg_replace('/<img[^>]*>/', '', $item->introtext);
 			}
 
-			$results                 = $app->triggerEvent('onContentAfterDisplay', array('com_content.article', &$item, &$params, 1));
+			$results                 = $app->triggerEvent('onContentAfterTitle', array('mod_articles_news.article', &$item, &$params, 1));
 			$item->afterDisplayTitle = trim(implode("\n", $results));
 
-			$results                    = $app->triggerEvent('onContentBeforeDisplay', array('com_content.article', &$item, &$params, 1));
+			$results                    = $app->triggerEvent('onContentBeforeDisplay', array('mod_articles_news.article', &$item, &$params, 1));
 			$item->beforeDisplayContent = trim(implode("\n", $results));
 		}
 
